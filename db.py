2633d360db9efa3bbd30d6005cf1a1059e70eb68
Laercio Pereira <laercio.pereira@objective.com.br>
2020-11-29 20:43:38 -0300

    fix warnings

--- db.py
+++ db.py
@@ -1,19 +1,22 @@
 import os
 import sqlite3
-from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
-from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 
 _db_ = os.getenv("HOME") + "/.kv.db"
 
 
 class DataBase():
+    """ Abstraction of the database using sqlite """
 
     def __init__(self):
+        """ Create de data base if not exists """
+
         connection = sqlite3.connect(_db_)
         statement = '''CREATE TABLE IF NOT EXISTS KV ( KEY TEXT NOT NULL, VALUE TEXT NOT NULL );'''
         connection.execute(statement)
 
     def execute_statement(self, statement):
+        """ Execute a query, returning dataset and commit the statemente """
+
         connection = sqlite3.connect(_db_)
         result = connection.execute(statement)
         connection.commit()
