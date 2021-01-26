e8271a1ac618511099e612393832ead666aa3869
Laercio Pereira <laercio.pereira@objective.com.br>
2021-01-26 00:11:17 -0300

    add suport to persist tags but not implemented yet

--- db.py
+++ db.py
@@ -1,7 +1,9 @@
+import logging
 import os
 import sqlite3
 
 _db_ = os.getenv("HOME") + "/.kv.db"
+_LOGGER_ = logging.getLogger(__name__)
 
 
 class DataBase():
@@ -11,8 +13,24 @@ class DataBase():
         """ Create de data base if not exists """
 
         connection = sqlite3.connect(_db_)
-        statement = '''CREATE TABLE IF NOT EXISTS KV ( KEY TEXT NOT NULL, VALUE TEXT NOT NULL );'''
-        connection.execute(statement)
+
+        statementVersionTable = '''CREATE TABLE IF NOT EXISTS DB_VERSION ( VERSION INTEGER NOT NULL );'''
+        connection.execute(statementVersionTable)
+
+        version = connection.execute('select VERSION from DB_VERSION').fetchone()
+        _LOGGER_.debug("VERSION = {}".format(version))
+
+        if version is None:
+            self.execute_statement('''CREATE TABLE IF NOT EXISTS KV ( KEY TEXT NOT NULL, VALUE TEXT NOT NULL );''')
+            self.execute_statement('''insert into DB_VERSION values (1);''')
+
+        if version[0] is 1:
+            self.execute_statement('''ALTER TABLE KV ADD COLUMN TAGS TEXT NULL;''')
+            self.execute_statement('''update DB_VERSION set VERSION = 2;''')
+
+        version = connection.execute('select VERSION from DB_VERSION').fetchone()
+        if version[0] is not 2:
+            _LOGGER_.debug("VERSION WRONG = {}".format(version))
 
     def execute_statement(self, statement):
         """ Execute a query, returning dataset and commit the statemente """
